3827f7c6b6d419439c779236b90ab9df70afc8e9
shubhamD88 <94123771+shubhamD88@users.noreply.github.com>
2022-01-05 13:32:23 +0530

    Add files via upload

---
+++ project3.py
@@ -0,0 +1,10 @@
+#printing positive number 
+list1=[12,-7,5,64,-14]
+list2=[12,14,-95,3]
+for i in range(len(list1)):
+    if(list1[i] >=0):
+        print(list1[i],end="\t")
+print("\n")
+for i in range(len(list2)):
+    if(list2[i] >=0):
+         print(list2[i],end="\t")
